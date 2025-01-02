553b1121090792b350b3063615346839e410e552
Shahriar Rahman Anik <122194751+sranik28@users.noreply.github.com>
2025-01-02 11:16:03 +0600

    patient interface done

---
+++ src/app/modules/Patient/patient.interface.ts
@@ -0,0 +1,10 @@
+export type TPatient = {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    role: string;
+    contactNo: string;
+    address: string;
+    profileImage: string;
+};
